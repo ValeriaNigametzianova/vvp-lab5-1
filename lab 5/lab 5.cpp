d36e3004ecfc30a363bc8f88fb95cb10401d9010
Валерия <valeria103020@gmail.com>
2020-10-14 15:38:54 +0300

    Добавьте файлы проекта.

---
+++ lab 5/lab 5.cpp
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+
+float L(int a, int b,int c, int d)
+{
+	float y;
+	y =(float) sqrt(pow(b - a, 2) + pow(d - c, 2));
+	return y;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int x1, x2, y1, y2;
+	float l;
+	printf("Введите координаты точки А: ");
+	scanf_s("%d%d" , &x1, &y1);
+	printf("Введите координаты точки B: ");
+	scanf_s("%d%d", &x2, &y2);
+	l = L(x1, x2, y1, y2);
+	printf("Расстояние между точками A и B равно %lf", l);
+	return 0;
+}
